f07514167aac0ea57abcaaf6cdd2a322fce9667f
idanmr <111880282+idanmr@users.noreply.github.com>
2024-06-06 15:11:28 +0300

    Exceptions

--- main.py
+++ main.py
@@ -1,28 +1,62 @@
-from Animal import Animal, Dog, Cat, Skunk, Unicorn, Dragon
+import string
+from UsernameContainsIllegalCharacter import *
+
+def check_input(username, password):
+    # Validate username
+    try:
+        valid_username(username)
+    except Exception as e:
+        print(e)
+        return False
+
+    # Validate password
+    try:
+        valid_password(password)
+    except Exception as e:
+        print(e)
+        return False
+
+    print("OK")
+    return True
 
 
-zoo_lst = [
-    Dog("Brownie", 10),
-    Cat("Zelda", 3),
-    Skunk("Stinky", 0),
-    Unicorn("Keith", 7),
-    Dragon("Lizzy", 1450)
-]
+def valid_username(username):
+    if len(username) < 3:
+        raise UsernameTooShort(username)
+    if len(username) > 16:
+        raise UsernameTooLong(username)
+    for index in range(len(username)):
+        char = username[index]
+        if not (char.isalnum() or char == '_'):
+            raise UsernameContainsIllegalCharacter(char, index)
+    return True
 
-doggo = Dog("Doggo", 80)
-kitty = Cat("Kitty", 80)
-stinky_jr = Skunk("Stinky Jr.", 80)
-clair = Unicorn("Clair", 80)
-mcfly = Dragon("McFly", 80)
 
-zoo_lst.extend([doggo, kitty, stinky_jr, clair, mcfly])
+def valid_password(password):
+    if len(password) < 8:
+        raise PasswordTooShort(password)
+    if len(password) > 40:
+        raise PasswordTooLong(password)
 
-for animal in zoo_lst:
-    if animal.is_hungry():
-        print(type(animal).__name__ + " " + animal.name)
-        while animal.is_hungry():
-            animal.feed()
-    animal.talk()
-    animal.special_method()
+    if not any(char.isupper() for char in password):
+        raise UppercaseMissing(password)
+    if not any(char.islower() for char in password):
+        raise LowercaseMissing(password)
+    if not any(char.isdigit() for char in password):
+        raise DigitMissing(password)
+    if not any(char in string.punctuation for char in password):
+        raise SpecialMissing(password)
 
-print(animal.zoo_name)
+    return True
+
+
+def main():
+    result = False
+    while not result:
+        username = input("Enter your username")
+        password = input("Enter your password")
+        result = check_input(username, password)
+
+
+if __name__ == "__main__":
+    main()
